e64bd8d3a9d8dcfa3c6c0e0dd86e7a5448fcbd9b
yangfan <seanyang@lilithgames.com>
2019-10-15 19:46:04 +0800

    绘制圆算法和吃鸡毒圈算法代码（待整理）

---
+++ MyTools/Assets/Scripts/DrawHelper.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawHelper
+{
+    private List<int> levelList = new List<int>();
+
+    private int level = 0;
+
+    private int interval = 2;    //每次时间间隔 以秒为单位
+
+    private UnityEngine.Random random = new UnityEngine.Random();
+
+    public List<int> LevelList
+    {
+        get
+        {
+            return levelList;
+        }
+    }
+
+    public int Level
+    {
+        get
+        {
+            return level;
+        }
+
+        set
+        {
+            level = value;
+        }
+    }
+
+    public int Interval
+    {
+        get
+        {
+            return interval;
+        }
+    }
+
+    public DrawHelper()
+    {
+        init();
+    }
+
+    private void init()
+    {
+        levelList.Add(600);
+        levelList.Add(400);
+        levelList.Add(240);
+        levelList.Add(130);
+        levelList.Add(70);
+        levelList.Add(30);
+        levelList.Add(15);
+    }
+
+    //public void drawCircle(Pen pen, PointF point, float radius)
+    //{
+    //    //x, y 这个点为 把整个圆（椭圆）包含在其中的最小正方形（矩形）的左上角那个点
+    //    //DrawEllipse 函数中width和height指的是直径
+
+    //    Rectangle mRect = new Rectangle((int)(point.X - radius), (int)(point.Y - radius), (int)(radius * 2), (int)(radius * 2));
+
+    //    mGraphics.DrawEllipse(pen, mRect);
+    //}
+
+    /// <summary>
+    /// 在圆心为point，半径为radius的圆内，产生一个半径为radius_inner的圆的圆心
+    /// </summary>
+    /// <param name="point">外圆圆心</param>
+    /// <param name="radius_outer">外圆半径</param>
+    /// <param name="radius_inner">内圆半径</param>
+    /// <returns>内圆圆心</returns>
+    public Vector2 PointOfRandom(Vector2 point, float radius_outer, float radius_inner)
+    {
+        int x = Random.Range((int)(point.x - (radius_outer - radius_inner)), (int)(point.x + (radius_outer - radius_inner)));
+        int y = Random.Range((int)(point.y - (radius_outer - radius_inner)), (int)(point.y + (radius_outer - radius_inner)));
+
+        while (!isInRegion(x - point.x, y - point.y, radius_outer - radius_inner))
+        {
+            x = Random.Range((int)(point.x - (radius_outer - radius_inner)), (int)(point.x + (radius_outer - radius_inner)));
+            y = Random.Range((int)(point.y - (radius_outer - radius_inner)), (int)(point.y + (radius_outer - radius_inner)));
+        }
+
+        Vector2 p = new Vector2(x, y);
+        return p;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="x_off">与大圆圆心的x方向偏移量</param>
+    /// <param name="y_off">与大圆圆心的y方向偏移量</param>
+    /// <param name="distance">大圆与小圆半径的差</param>
+    /// <returns>判断点是否在范围内</returns>
+    public bool isInRegion(float x_off, float y_off, float distance)
+    {
+        if (x_off * x_off + y_off * y_off <= distance * distance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 判断两个圆是否重合，或者是相内切
+    /// </summary>
+    /// <param name="p_outer">外圆圆心</param>
+    /// <param name="r_outer">外圆半径</param>
+    /// <param name="p_inner">内圆圆心</param>
+    /// <param name="r_inner">内圆半径</param>
+    /// <returns>是否相内切</returns>
+    public bool isIntersect(Vector2 p_outer, float r_outer, Vector2 p_inner, float r_inner)
+    {
+        //判定条件：两圆心的距离 + r_inner = r_outer
+
+        float distance = (float)Mathf.Sqrt((p_outer.x - p_inner.x) * (p_outer.x - p_inner.x) + (p_outer.y - p_inner.y) * (p_outer.y - p_inner.y));
+
+        if (distance + r_inner >= r_outer)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ MyTools/Assets/Scripts/PoisonTest.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonTest : MonoBehaviour
+{
+    public GameObject poison;
+    public GameObject safe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init_bg();
+        timer2_Tick();
+        isStart = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isStart)
+        {
+            return;
+        }
+        if(!timer2enable)
+        {
+            timer1_Tick();
+        }else
+        {
+            timer2_Tick();
+        }
+        
+    }
+
+    //理想点，但是用于画图的话还要再减去圆的r
+    private Vector2 mPoint_outer;
+
+    private Vector2 mPoint_inner;
+
+    private float mRadius_outer = 0;
+
+    private float mRadius_inner = 0;
+
+    private DrawHelper mDrawHelper = null;
+
+    public bool isStart = false;
+
+    private bool timer1enable = false;
+    private bool timer2enable = false;
+
+    private void init_bg()
+    {
+
+
+        mPoint_outer = Vector2.zero;
+
+        mDrawHelper = new DrawHelper();
+
+        mRadius_outer = mDrawHelper.LevelList[0];
+    }
+
+
+    private void timer1_Tick()
+    {
+
+        mRadius_outer-=(float)10/30;
+
+
+        if (!mDrawHelper.isIntersect(mPoint_outer, mRadius_outer, mPoint_inner, mRadius_inner))
+        {
+            poison.transform.localScale = new Vector3(mRadius_outer, mRadius_outer, 1);
+            poison.transform.localPosition = new Vector3(mPoint_outer.x/60, mPoint_outer.y/60, 0);
+        }
+        else
+        {
+            if (mRadius_outer != mRadius_inner)  //外圈和内圈圆心重合,半径相同
+            {
+                // 图三过程
+                // k = y/x
+                // y = kx
+                // x^2+y^y = 1
+                // x^2 = 1/(k^2+1)
+                float k = (mPoint_outer.y - mPoint_inner.y) / (mPoint_outer.x - mPoint_inner.x);
+
+                float x_off = 1 * (float)Mathf.Sqrt(1 / (k * k + 1));
+
+                // k<0  x+x_off
+                mPoint_outer.x += 1 * (mPoint_outer.x < mPoint_inner.x ? 1 : -1) * x_off;
+                mPoint_outer.y = k * (mPoint_outer.x - mPoint_inner.x) + mPoint_inner.y;
+
+                //reCalcPoint_outer(mPoint_outer, mPoint_inner);
+
+                poison.transform.localScale = new Vector3(mRadius_outer, mRadius_outer, 1);
+                poison.transform.localPosition = new Vector3(mPoint_outer.x/60, mPoint_outer.y/60, 0);
+            }
+            else
+            {
+                poison.transform.localScale = new Vector3(mRadius_outer, mRadius_outer, 1);
+                poison.transform.localPosition = new Vector3(mPoint_outer.x/60, mPoint_outer.y/60, 0);
+                timer2enable = true;
+              
+            }
+        }
+    }
+
+    /// <summary>
+    /// 后台时间表，用于毒圈（外圈）和内圈重合后，暂停时间以及生成下一个内圈圆心和内圈半径
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void timer2_Tick()
+    {
+        if (mDrawHelper.Level < mDrawHelper.LevelList.Count - 1 && mRadius_outer == mDrawHelper.LevelList[mDrawHelper.Level])
+        {
+            mDrawHelper.Level++;
+            mRadius_inner = mDrawHelper.LevelList[mDrawHelper.Level];
+            mPoint_inner = mDrawHelper.PointOfRandom(mPoint_outer, mRadius_outer, mRadius_inner);
+
+            safe.transform.localScale = new Vector3(mRadius_inner, mRadius_inner, 1);
+            safe.transform.localPosition = new Vector3(mPoint_inner.x/60, mPoint_inner.y/60, 0);
+        }
+        else
+        {
+            timer1enable = false;
+            timer2enable = false;
+        }
+    }
+
+}
